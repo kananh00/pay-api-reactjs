43f25b1e4c474858350c6b79defa84cece75eb29
kenan00 <kenan.huseynli44@gmail.com>
2021-08-04 20:20:50 +0400

    add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import './component-styles/formStyle.css';
+import checkedIcon from '../assets/shared/desktop/icon-check.svg';
 
 class Form extends Component{
     state = {
         fields: {},
         errors: {},
-        inputisValid: true
+        inputisValid: true,
+        checkboxIsChecked: false
     }
-
+    onCheckboxClick = () =>{
+        if(!this.state.checkboxIsChecked){
+            this.setState({checkboxIsChecked: true})
+        }
+        else{
+            this.setState({checkboxIsChecked: false})
+        }
+        console.log(this.state.checkboxIsChecked)
+    }
     handleInputValidation(){
         let fields = this.state.fields;
         let errors = {};
@@ -70,8 +80,60 @@ class Form extends Component{
         return(
             <div className="form_contact">
                 <form onSubmit = {this.contactSubmit.bind(this)}>
-                    {this.state.inputisValid ?  <input onChange={this.handleChange.bind(this, this.props.name)} value={this.state.fields[this.props.name]} className="form_input" type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}/> : <input onChange={this.handleChange.bind(this, this.props.name)} value={this.state.fields[this.props.name]} className="form_input error_input" type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}/>}
-                    <span className="error_msg">{this.state.errors[this.props.name]}</span>
+                    {/* {this.state.inputisValid ?  <input onChange={this.handleChange.bind(this, this.props.name)} value={this.state.fields[this.props.name]} className="form_input" type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}/> : <input onChange={this.handleChange.bind(this, this.props.name)} value={this.state.fields[this.props.name]} className="form_input error_input" type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}/>}
+                    <span className="error_msg">{this.state.errors[this.props.name]}</span> */}
+                    <input 
+                        onChange={this.handleChange.bind(this, "name")} 
+                        value={this.state.fields["name"]} 
+                        className={`form_input ${this.state.inputisValid ? "" : "error_input"}`}
+                        type="text" 
+                        name="name" 
+                        placeholder="Name"
+                    />
+                    <span className="error_msg">{this.state.errors["name"]}</span>
+                    <input 
+                        onChange={this.handleChange.bind(this, "email")} 
+                        value={this.state.fields["email"]} 
+                        className={`form_input ${this.state.inputisValid ? "" : "error_input"}`}
+                        type="email" 
+                        name="email"
+                        placeholder="Email Adress"
+                    />
+                    <span className="error_msg">{this.state.errors["email"]}</span>
+                    <input 
+                        onChange={this.handleChange.bind(this, "company")} 
+                        value={this.state.fields["company"]} 
+                        className={`form_input ${this.state.inputisValid ? "" : "error_input"}`}
+                        type="text" 
+                        name="company"
+                        placeholder="Company Name"
+                    />
+                    <span className="error_msg">{this.state.errors["company"]}</span>
+                    <input 
+                        onChange={this.handleChange.bind(this, "title")} 
+                        value={this.state.fields["title"]} 
+                        className={`form_input ${this.state.inputisValid ? "" : "error_input"}`}
+                        type="text" 
+                        name="title"
+                        placeholder="Title"
+                    />
+                    <span className="error_msg">{this.state.errors["title"]}</span>
+                    <textarea
+                        rows='3'
+                        onChange={this.handleChange.bind(this, "message")} 
+                        value={this.state.fields["message"]} 
+                        className={`form_input ${this.state.inputisValid ? "" : "error_input"}`}
+                        type="text"
+                        name="message"
+                        placeholder="Message"
+                    />
+                    <span className="error_msg">{this.state.errors["message"]}</span>
+                    <div className="checkLine">
+                        {!this.state.checkboxIsChecked ? <div onClick={this.onCheckboxClick} className="checkbox"></div> : <div onClick={this.onCheckboxClick} className="checkbox check_clicked"><img className="checkbox_icon" src={checkedIcon} alt="check icon"/></div>}
+                        <p className="checkbox_text">Stay up-to-date with company announcements and updates to our API</p>
+                    </div>
+                    <input type="submit" className="submit_btn" value="Submit"/>
+                    {/* <ButtonMain btnName='Submit' color='#36536B' padding='16px 50px 14px 50px' marginBottom='75px'/> */}
                 </form>
             </div>
         )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,10 @@
 import React, {Component} from 'react';
 import './contactStyle.css';
 import Form from '../components/Form';
-import ButtonMain from '../components/ButtonMain';
 import Corparation from '../components/Corparation';
 import { ReadyToStart } from '../components/ReadyToStart';
-import checkedIcon from '../assets/shared/desktop/icon-check.svg';
 
 class Contact extends Component{
-    state = {
-            checkboxIsChecked: false
-    }
-    onCheckboxClick = () =>{
-        if(!this.state.checkboxIsChecked){
-            this.setState({checkboxIsChecked: true})
-        }
-        else{
-            this.setState({checkboxIsChecked: false})
-        }
-        console.log(this.state.checkboxIsChecked)
-    }
     render(){
         return(
             <div className="contact page">
@@ -31,18 +17,7 @@ class Contact extends Component{
                         </div>
                         <div className="col-12 col-lg-6 mt-lg-4">
                             <div className="form_part">
-                                <Form type='text' rows='1' name='name' placeholder="Name"/>
-                                <Form type='email' rows='1' name='email' placeholder="Email Adress"/>
-                                <Form type='text' rows='1' name='company' placeholder="Company Name"/>
-                                <Form type='text' rows='1' name='title' placeholder="Title"/>
-                                <Form type='text' rows='3' name='message' placeholder="Message"/>
-                                
-                                <div className="checkLine">
-                                    {!this.state.checkboxIsChecked ? <div onClick={this.onCheckboxClick} className="checkbox"></div> : <div onClick={this.onCheckboxClick} className="checkbox check_clicked"><img className="checkbox_icon" src={checkedIcon} alt="check icon"/></div>}
-                                    <p className="checkbox_text">Stay up-to-date with company announcements and updates to our API</p>
-                                </div>
-
-                                <ButtonMain btnName='Submit' color='#36536B' padding='16px 50px 14px 50px' marginBottom='75px'/>
+                                <Form/>
                             </div>
                         </div>
                         <div className="col-12 col-lg-6 d-lg-flex align-items-lg-center px-lg-4">
